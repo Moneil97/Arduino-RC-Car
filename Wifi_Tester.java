9934ede7c70a4ae5947e961cda7612c2b78a01cc
Moneil <cam8059@yahoo.com>
2015-01-31 17:24:11 -0600

    Added GUI, ability to type in messages

--- Wifi_Tester.java
+++ Wifi_Tester.java
@@ -1,120 +1,109 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
 import hypermedia.net.UDP;
 
-//enum Controls{
-//	left(37), right(39), straight(36), forward(38), reverse(40), still(41);
-//	
-//	private final int id;
-//	Controls(int id) { this.id = id; }
-//    public int getValue() { return id; }
-//}
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
 
 public class Wifi_Tester extends JFrame{
 
 	private static final long serialVersionUID = 1L;
+	private JTextField inputTextbox;
+	private String ip = "192.168.1.202";  // the remote IP address
+	private int port  = 8888;    // the destination port
+	private JPanel outputBox;
+	private JButton sendButton;
+	private JScrollPane scrollPane;
 
 	public Wifi_Tester() {
-		
+		//Connect to Arduino Ethernet Board
 		UDP udp = new UDP(this, 6000);
 		udp.broadcast(true);
 		udp.listen(true);
 		
-		setSize(200,200);
+		//Setup JFrame
+		setSize(322,269);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		setVisible(true);
 		
+		JPanel lowerPanel = new JPanel();
+		getContentPane().add(lowerPanel, BorderLayout.SOUTH);
+		lowerPanel.setLayout(new BorderLayout(0, 0));
 		
-		addKeyListener(new KeyListener() {
-			
-			private String ip = "192.168.1.202";  // the remote IP address
-			private int port  = 8888;    // the destination port
-			private boolean leftHeld, rightHeld, forwardHeld, reverseHeld;
+		inputTextbox = new JTextField();
+		inputTextbox.addActionListener(new ActionListener() {
 			
 			@Override
-			public void keyTyped(KeyEvent e) {}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				
-				int keycode = e.getKeyCode();
-				
-				if (keycode == Controls.left.getValue()){
-					leftHeld = false;
-					udp.send(String.valueOf(Controls.straight.getValue()), ip, port);
-				}
-				else if (keycode == Controls.right.getValue()){
-					rightHeld = false;
-					udp.send(String.valueOf(Controls.straight.getValue()), ip, port);
-				}
-				else if (keycode == Controls.forward.getValue()){
-					forwardHeld = false;
-					udp.send(String.valueOf(Controls.still.getValue()), ip, port);
-				}
-				else if (keycode == Controls.reverse.getValue()){
-					reverseHeld = false;
-					udp.send(String.valueOf(Controls.still.getValue()), ip, port);
-				}
-				else{
-					System.err.println("Released Invalid Key: " + keycode);
-					return;
-				}
-				
-				System.out.println("Released: " + keycode);
-			}
-			
-			@Override
-			public void keyPressed(KeyEvent e) {
-				
-				int keycode = e.getKeyCode();
-				
-				if (!leftHeld && keycode == Controls.left.getValue()){
-					leftHeld = true;
-					udp.send(String.valueOf(keycode), ip, port);
-				}
-				else if (!rightHeld  && keycode == Controls.right.getValue()){
-					rightHeld = true;
-					udp.send(String.valueOf(keycode), ip, port);
-				}
-				else if (!forwardHeld  && keycode == Controls.forward.getValue()){
-					forwardHeld = true;
-					udp.send(String.valueOf(keycode), ip, port);
-				}
-				else if (!reverseHeld  && keycode == Controls.reverse.getValue()){
-					reverseHeld = true;
-					udp.send(String.valueOf(keycode), ip, port);
-				}
-				else{
-					//System.err.println("Pressed Invalid Key: " + keycode);
-					return;
-				}
-				System.out.println("Pressed Key: " + keycode);
+			public void actionPerformed(ActionEvent e) {
+				sendButton.doClick();
 			}
 		});
+		lowerPanel.add(inputTextbox, BorderLayout.CENTER);
+		inputTextbox.setColumns(10);
 		
+		sendButton = new JButton("Send");
+		sendButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				//Add Sent message to GUI
+				JLabel temp = new JLabel(inputTextbox.getText());
+				temp.setForeground(Color.black);
+				outputBox.add(temp);
+				outputBox.revalidate();
+				
+				//Send string to Arduino Ethernet Board
+				udp.send(inputTextbox.getText(), ip, port);
+				
+				inputTextbox.setText("");
+			}
+		});
+		lowerPanel.add(sendButton, BorderLayout.EAST);
+		
+		scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		outputBox = new JPanel();
+		scrollPane.setViewportView(outputBox);
+		outputBox.setLayout(new GridLayout(0,1));
+		setVisible(true);
+		
+		//Tell UDP object the name of the method to call when it receives feedback from Arduino
 		udp.setReceiveHandler("receive");
 	}
 	
+	//Called when Arduino sends feedback through Ethernet
 	public void receive(byte[] message) {
 		
-		System.out.println("\nReceived:");
-		System.out.println("--------------------");
-		
+		String rec = "";
 		for (byte b : message){
-			System.out.print((char) b);
+			rec += (char) b;
 		}
-		System.out.println("\n--------------------");
+		
+		//Add message to GUI
+		JLabel temp = new JLabel(rec);
+		temp.setForeground(Color.blue);
+		outputBox.add(temp);
+		outputBox.revalidate();
 	 }
 	
 	public static void say(Object s){
 		System.out.println("System: " + s);
 	}
 	
-
 	public static void main(String[] args) {
 		new Wifi_Tester();
 	}
