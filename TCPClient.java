5f4bcdfe64c4c0cb570c38b0f98522f8b6b30a35
Moneil <cam8059@yahoo.com>
2015-02-01 00:53:11 -0600

    Flips LED through TCP Connection

---
+++ TCPClient.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class TCPClient {
+
+	private static Socket sock;
+
+	public TCPClient() throws IOException {
+		
+		sock = new Socket("192.168.1.171",1336);
+		System.out.println(sock);
+		
+		for (int i=0; i < 5; i++){
+			send("on");
+			say("on");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			send ("off");
+			say("off");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		sock.close();
+	}
+	
+	public static void say(Object s) {
+		System.out.println(s);
+	}
+
+	public static void main(String[] args) throws Exception{
+		new TCPClient();
+	}
+	
+	public void send(String s) throws IOException{
+		//Stay under 50 ish chars
+		sock.getOutputStream().write(("/S" + s + "/E").getBytes());
+		sock.getOutputStream().flush();
+	}
+
+}
